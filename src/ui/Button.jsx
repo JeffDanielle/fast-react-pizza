2e8e85147ab724986e3ab2cd30201c1d64c2556a
JeffDanielle <bayotjeff@gmail.com>
2024-04-07 18:38:25 +0800

    308. Styling the Menu -18min

--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router-dom"
 
-function Button({ children, disabled, to }) {
-    const className = "bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition ease -in -out delay - 50 focus: outline - none focus:ring focus: ring - yellow - 300focus:ring - offset - 2 disabled: cursor - not - allowed sm: px - 6 sm: py - 4"
+function Button({ children, disabled, to, type }) {
+    const base = "bg-yellow-400 uppercase font-semibold text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300 transition ease-in-out delay-50 focus:outline-none focus:ring focus:ring-yellow-300 focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4"
+
+
+    const styles = {
+        primary: base + ' px-4 py-3 md:px-6 md:py-4',
+        small: base + ' px-4 py-2 md:px-5 md:py-2.5 text-xs'
+    }
+
     if (to)
-        return <Link className={className} to={to}>{children}</Link>
+        return <Link className={styles[type]} to={to}>{children}</Link>
     return (
-        <button disabled={disabled} className="bg-yellow-400 uppercase font-semibold
-               text-stone-800 py-3 px-4 inline-block rounded-full hover:bg-yellow-300
-               transition ease-in-out delay-50 focus:outline-none focus:ring focus:ring-yellow-300 
-               focus:ring-offset-2 disabled:cursor-not-allowed sm:px-6 sm:py-4">
+        <button disabled={disabled} className={styles[type]}>
             {children}
         </button>
     )
